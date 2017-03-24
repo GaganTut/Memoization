4122c6bd4f74179e1efc7d38c5ecb33c5c9931ec
GaganTut <gstut@live.com>
2017-03-24 09:52:19 -1000

    Using bracket notation and added queryselectorall

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ var domMemoize = function() {
   var cache = {};
 
   function saveQuery(queryVal) {
-    cache.queryVal = document.querySelector(queryVal);
+    cache[queryVal] = document.querySelector(queryVal);
   }
 
   function useQuery(queryVal) {
-    if (cache.queryVal !== undefined) {
-      return cache.queryVal;
+    if (cache[queryVal] !== undefined) {
+      return cache[queryVal];
     } else {
       return false;
     }
   }
 
+  function saveQueryCollection(queryVal) {
+    cache[queryVal] = document.querySelectorAll(queryVal);
+  }
 
   return {
     saveQuery,
